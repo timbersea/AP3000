fef5a137c9a424c70a471b894c1c56c596b52c0d
timbersea <550889936@qq.com>
2025-10-30 21:55:33 +0800

    update:单元测试

--- src/test/java/com/an/StandardTest.java
+++ src/test/java/com/an/StandardTest.java
@@ -2,7 +2,9 @@ package com.an;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Spring Boot 标准单元测试模板
